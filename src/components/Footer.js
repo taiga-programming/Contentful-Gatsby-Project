07128f4ae1ecd9832435c8e769083f418e50cdd3
taiga <taiga.similarities@gmail.com>
2020-08-27 12:42:14 +0900

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,40 @@
+import React from "react"
+import styles from "../css/footer.module.css"
+import links from "../constants/links"
+import socialIcons from "../constants/social-icons"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.links}>
+        {links.map((item, index) => {
+          return (
+            <AniLink fade key={index} to={item.path}>
+              {item.text}
+            </AniLink>
+          )
+        })}
+      </div>
+      <div className={styles.icons}>
+        {socialIcons.map((item, index) => {
+          return (
+            <a
+              key={index}
+              href={item.url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {item.icon}
+            </a>
+          )
+        })}
+      </div>
+      <div className={styles.copyright}>
+       © This site was made by taiga  {new Date().getFullYear()} 
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
